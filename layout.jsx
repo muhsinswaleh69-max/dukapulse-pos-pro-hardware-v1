f74e72955d744aa904ed9a41c5ddf10b632ad4ec
muhsinswaleh69-max <muhsinswaleh69@gmail.com>
2026-09-23 18:16:21 +0300

    Delete layout.jsx

--- layout.jsx
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: "DukaPulse Pro Hardware" };
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{margin:0, background:"#f5f7f3", fontFamily:"sans-serif"}}>{children}</body>
-    </html>
-  );
-}
